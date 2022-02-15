8a650de21fee64798f9516b0bd8162fe098bacff
linkedin <n8ebel@goobar.dev>
2022-02-14 19:37:43 -0700

    End of 02_01

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
 
 fun main() {
+    var ourFirstVariable = true
 
+    println(ourFirstVariable)
+
+    ourFirstVariable = false
+    println(ourFirstVariable)
+
+    println(ourFirstVariable == true)
+    println(ourFirstVariable == false)
 }
