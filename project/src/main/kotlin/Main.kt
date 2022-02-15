2d81d0f1e6fd6ef1a3839244fc32db28f43dd79b
linkedin <n8ebel@goobar.dev>
2022-02-14 19:43:38 -0700

    Prepare for 02_02

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,12 +1,4 @@
 
 fun main() {
-    var ourFirstVariable = true
-
-    println(ourFirstVariable)
-
-    ourFirstVariable = false
-    println(ourFirstVariable)
-
-    println(ourFirstVariable == true)
-    println(ourFirstVariable == false)
+    
 }
