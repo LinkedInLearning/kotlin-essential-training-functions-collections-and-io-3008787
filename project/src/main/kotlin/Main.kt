dfe35379dde96130667f5d7f707e067d9d3ba322
linkedin <n8ebel@goobar.dev>
2022-02-14 20:25:47 -0700

    Prep for 02_4

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,42 +1,4 @@
 
 fun main() {
-    val aChar: Char = 'a'
-    val aNumericChar = '1'
-    val newLineChar = '\n'
-
-    //val concatOption1 = 'a' + 'b'
-
-    //val concatOption2 = 'ab'
-
-    val aString: String = "Hello Kotlin!"
-    val escaped = "Hello \n World"
-    println(escaped)
-
-    val raw = """
-      >>Some
-        multiline
-     text
-      >>is going on
-       here   
-    """.trimMargin(">>")
-    println(raw)
-
-    val concatString = "Hello" + " Kotlin!"
-
-    val concatNumber = "The number " + 10
-    println(concatNumber)
-
-
-    val aNumber = 10
-    val concatTemplate = "The number ${1 + aNumber}"
-    println(concatTemplate)
-
-
-    val someString = "  "
-    println(someString.isNotBlank())
-
-    val anotherString = "Hello Kotlin World!"
-    anotherString.contains("Kotlin")
-
     
 }
