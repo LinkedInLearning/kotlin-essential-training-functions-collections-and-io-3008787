b0857fda7b1a935538dbba17581a6070bf5ff3fe
linkedin <n8ebel@goobar.dev>
2022-02-14 20:01:31 -0700

    Prep for 02_03

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,38 +1,4 @@
 
 fun main() {
-    val aInt: Int = 0
-    val anotherInt = 0
-
-    val aByte: Byte = 0
-
-    val aShort: Short = 0
-
-    val aLong: Long = 0
-
-    val inferredInt = 10
-    val inferedLong = 1000000000
-
-    val anotherLong = 10L
-
-    val unsignedInt: UInt = 0U
-
-    val unisgnedLong = 0UL
-
-    val aDouble: Double = 5.5
-    val aFloat: Float = 5.5f
-
-    val inferredDouble = 5.5
-    val inferredFloat = 5.5f
-
-    println(5==4)
-    println(10f > 1)
-    println(10.1f <= 5.2)
-
-    println(10.5.toInt())
-    10.9.toFloat()
-    5.toULong()
-
-
-    println(100_000_000.toByte())
-
+    
 }
