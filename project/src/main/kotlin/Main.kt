d94c2bf68e27ac64cdd31e43f62565e3dd9e945d
linkedin <n8ebel@goobar.dev>
2022-02-16 08:57:42 -0700

    04_01

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,20 @@
 
 
 fun main() {
+    var someVariable = 100
 
+//    when {
+//        someVariable > 3 -> println("The value was greater than 3")
+//        someVariable > 2 -> println("The value was greater than 2")
+//        else -> {
+//            println("Not greater")
+//        }
+//    }
+
+    when (someVariable) {
+        0,1 -> println("The value was 0 or 1")
+        2 -> println("The value is 2")
+        3 -> println("The value is 3")
+        in 4..Int.MAX_VALUE -> println("The value was greater than 3")
+    }
 }
