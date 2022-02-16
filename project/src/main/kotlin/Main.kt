b086cd15793dfdcfdb722afe49e8c0aecaaefa70
linkedin <n8ebel@goobar.dev>
2022-02-16 08:13:44 -0700

    03_04

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 
 
+fun printCalculatedValue(value1: Int, value2: Int, calculator: (Int, Int) -> Int) {
+    println("The value is: ${calculator(value1, value2)}")
+}
+
 fun main() {
-    
+    printCalculatedValue(2, 2) { value1, value2 ->
+        value1 + value2
+    }
+
+    printCalculatedValue(2, 2, { value1, value2 ->
+        value1 - value2
+    })
 }
