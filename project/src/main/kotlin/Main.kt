2350dbfa075cd2a3fd5318605de477c81c4a4418
linkedin <n8ebel@goobar.dev>
2022-02-15 21:17:55 -0700

    03_03

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,15 @@
 
+var greetingFunction: (String) -> String = { thingToGreet ->
+    "Hello $thingToGreet"
+}
+
 fun main() {
-    
+    println(greetingFunction("World"))
+    println(greetingFunction("Kotlin"))
+
+    greetingFunction = { thingToGreet ->
+        "Hello $thingToGreet!"
+    }
+
+    println(greetingFunction.invoke("Kotlin Programming"))
 }
