4d12ee60d7b17791278494053cd1d5c74fc13c4e
linkedin <n8ebel@goobar.dev>
2022-02-15 19:48:59 -0700

    Prep for 03_03b

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,11 +1,4 @@
 
-private fun printGreeting() = println("Hello Kotlin")
-
 fun main() {
-    fun getGreeting(
-        greeting: String = "Hello",
-        thingToGreet: String = "World"
-    ) = "$greeting $thingToGreet"
-
-    println(getGreeting(thingToGreet = "World", greeting = "Hey there"))
+    
 }
