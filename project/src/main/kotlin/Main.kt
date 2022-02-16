b18d16ebedbb3a1740fecdbdb5da1b364df42a0f
linkedin <n8ebel@goobar.dev>
2022-02-16 09:19:25 -0700

    04_02

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,20 @@
 
+fun getMessage(input: Int) = if (input > 3) {
+        "Greater than 3"
+    } else {
+        "Not greater than 3"
+    }
+
+fun getMessageWithWhen(input: Int) = when (input) {
+    3 -> "Value is 3"
+    else -> "Value is not 3"
+}
+
 
 fun main() {
-    
+    var someVariable = 0
+    val message = when (someVariable) {
+        3 -> "The value is 3"
+        else -> "The value is not 3"
+    }
 }
