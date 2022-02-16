0749c496496aa08f87cf83e150df52b20937b971
linkedin <n8ebel@goobar.dev>
2022-02-16 09:43:28 -0700

    Prep for 04_04

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,13 +1,5 @@
 
 
 fun main() {
-    val message = try {
-        throw IllegalStateException()
-        "The value is ${10 / 0}"
-    } catch (error: ArithmeticException) {
-        "Error was thrown"
-    } catch (error: java.lang.IllegalStateException) {
-        "Error was IllegalState"
-    }
-    println(message)
+    
 }
