f15ee42875bdd1dc05a934284b0353de3cc6c883
linkedin <n8ebel@goobar.dev>
2022-02-16 08:32:14 -0700

    03_06

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -6,6 +6,23 @@
 // function takes a formatting function parameter
 // function should print the result of the passed format function
 
-fun main() {
+fun printFormattedName(first: String, last: String, formatter: (String, String) -> String) {
+    println(formatter(first, last))
+}
 
+val basicFormatter: (String, String) -> String = { first, last ->
+    "$first $last"
+}
+
+val fancyFormatter: (String, String) -> String = { first, last ->
+    "first name is $first and last name is $last"
+}
+
+fun main() {
+    printFormattedName("Nate", "Ebel", basicFormatter)
+    printFormattedName("Nate", "Ebel", fancyFormatter)
+
+    printFormattedName("Nate", "Ebel") { first, last ->
+        "$last, $first"
+    }
 }
