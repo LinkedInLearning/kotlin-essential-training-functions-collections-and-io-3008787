574915728e3eca15adcacf5813c842d9fec16fc1
linkedin <n8ebel@goobar.dev>
2022-02-16 21:13:26 -0700

    Prep for 04_07

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,19 @@
+// Refactor getOutput() to use a when expression
+fun getOutput(input: Any?): String = if (input is Number) {
+    if (input !is Int) {
+        "Input was an Int"
+    } else {
+        "Input was a non-Int Number"
+    }
+}
+else if (input is String) { "Input was a String with length ${input.length}" }
+else if (input == null) { "Input was null" }
+else { "Input didn't match target inputs" }
 
 fun main() {
-    
+    println(getOutput(null))
+    println(getOutput(4))
+    println(getOutput(3.2))
+    println(getOutput("Hello Kotlin"))
+    println(getOutput('a'))
 }
