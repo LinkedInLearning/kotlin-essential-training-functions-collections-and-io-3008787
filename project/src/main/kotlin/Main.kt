68ff9e78feadac93e0ddbaf225e4899759a86362
linkedin <n8ebel@goobar.dev>
2022-02-16 21:44:14 -0700

    Prep for 05_01b

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,22 +1,13 @@
-// Refactor getOutput() to use a when expression
-
-fun getOutput(input: Any?): String = when (input) {
-    is Number -> {
-        if (input !is Int) {
-            "Input was an Int"
-        } else {
-            "Input was a non-Int Number"
-        }
-    }
-    is String -> "Input was a String with length ${input.length}"
-    null -> "Input was null"
-    else -> "Input didn't match target inputs"
-}
+import kotlin.random.Random
 
 fun main() {
-    println(getOutput(null))
-    println(getOutput(4))
-    println(getOutput(3.2))
-    println(getOutput("Hello Kotlin"))
-    println(getOutput('a'))
+    for (i in 0..4) {
+        val random = Random.nextInt()
+        val isEven = random.rem(2) == 1
+        when(isEven) {
+            true -> println("$random is even")
+            false -> println("$random is odd")
+        }
+    }
+
 }
