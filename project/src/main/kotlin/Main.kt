219f6ba836eb8478e09d573eb7c1f749b8a9600f
linkedin <n8ebel@goobar.dev>
2022-02-16 19:26:17 -0700

    04_04e

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
-
+fun checkType(input: Any?) {
+    when (input) {
+        null -> println("Input was null")
+        is String -> println("Input was a String of length ${input.length}")
+    }
+}
 
 fun main() {
-    
+    val aGenericVariable: Any = 5
+
+    checkType("null")
 }
