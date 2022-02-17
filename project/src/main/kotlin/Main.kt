d7230ba47c0c7406f44ae2bdf433cc0b3a373409
linkedin <n8ebel@goobar.dev>
2022-02-16 22:04:48 -0700

    05_02

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
 import kotlin.random.Random
 
+fun getRandomInt() = Random.nextInt()
+
 fun main() {
     for (i in 0..4) {
-        val random = Random.nextInt()
+        val random = getRandomInt()
         val isEven = random.rem(2) == 1
         when(isEven) {
             true -> println("$random is even")
