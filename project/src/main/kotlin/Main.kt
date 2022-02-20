51140b38b551d95155a75e9510b90255f448b233
linkedin <n8ebel@goobar.dev>
2022-02-20 13:29:01 -0700

    Prep for 07_02

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,12 +1,4 @@
 
 fun main(vararg args: String) {
-
-    if (args.isEmpty()) {
-        print("Please pass at least one argument")
-        return
-    }
-
-    println("First arg is ${args[0]}")
-
-    for (arg in args) println(arg)
+    
 }
