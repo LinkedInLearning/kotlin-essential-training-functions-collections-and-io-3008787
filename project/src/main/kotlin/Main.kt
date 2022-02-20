ca1f96ef1972c0fc972f01326ccd10ae0a139898
linkedin <n8ebel@goobar.dev>
2022-02-20 12:48:54 -0700

    Prep for 06_06

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
 
 fun main() {
+    // Using the provided test scores, identify the 3 students
+    // with the lowest test scores
+
     
 }
---
+++ project/src/main/kotlin/test-scores.kt
@@ -0,0 +1,13 @@
+
+val testScores = mapOf(
+    "123abc" to 88.1,
+    "124xyz" to 88.9,
+    "345abc" to 82.1,
+    "345bbc" to 72.1,
+    "34efbc" to 66.1,
+    "345abc" to 89.1,
+    "385agc" to 62.1,
+    "837abc" to 85.7,
+    "9459bc" to 94.3,
+    "nd2192" to 83.8,
+)
