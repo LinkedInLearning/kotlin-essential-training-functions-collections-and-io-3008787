235811a2e39ce8f8f84136df637f19ce966ab528
linkedin <n8ebel@goobar.dev>
2022-02-20 13:44:37 -0700

    Prep for 07_03

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,14 +1,4 @@
-import java.io.File
 
 fun main() {
-    print("Enter a filename: ")
-    val filename = readLine() ?: ""
-    println("You entered: $filename")
 
-    val isValidFile = File(filename).isFile
-    if (isValidFile) {
-        println("It is a valid file")
-    } else {
-        println("It is not a valid file")
-    }
 }
