48a86ed579a098768f6920491cf152642932e7e3
linkedin <n8ebel@goobar.dev>
2022-02-20 12:14:47 -0700

    06_04

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,44 @@
 
 fun main() {
-    
+    val readOnlyList = listOf(1, 2, 3)
+    val readOnlySet = setOf(1, 2, 3)
+    val readOnlyMap = mapOf(1 to "a", 2 to "b")
+
+    val mutableList = mutableListOf(1, 2, 3)
+    val mutableSet = mutableSetOf(1, 2, 3)
+    val mutableMap = mutableMapOf(1 to "a", 2 to "b")
+
+    mutableList.size
+    readOnlyMap.size
+
+    mutableSet.isEmpty()
+    readOnlyList.isNotEmpty()
+
+    for (element in readOnlySet) {
+        println(element)
+    }
+
+    mutableMap.forEach { key, value -> println(value) }
+
+    readOnlyList.first()
+    mutableSet.first()
+
+    mutableList.take(3)
+    readOnlySet.take(2)
+
+    mutableMap.filter { entry -> entry.key < 2 }
+    readOnlyList.filter { value -> value > 1 }
+
+    val languages = mapOf(
+        "kotlin" to 5,
+        "java" to 3,
+        "javascript" to 4,
+        "c++" to 2,
+        "python" to 6
+    )
+
+    languages.filter { it.value >= 4 }
+        .map { it.key }
+        .sorted()
+        .forEach { println(it) }
 }
