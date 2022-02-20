8885aea0c56ecbeed7170ca45c15e8433e5d61e9
linkedin <n8ebel@goobar.dev>
2022-02-20 13:06:06 -0700

    06_07

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -3,5 +3,9 @@ fun main() {
     // Using the provided test scores, identify the 3 students
     // with the lowest test scores
 
-    
+    testScores.toList()
+        .sortedBy { pair -> pair.second } // sort by test score
+        .map { pair -> pair.first } // map to student id
+        .take(3) // take 3 lowest values for the sorted list
+        .forEach { println(it) }
 }
