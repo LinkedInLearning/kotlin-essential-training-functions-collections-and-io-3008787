1b5ceb3927a1a2dfef896bef884139b3f401c4ad
linkedin <n8ebel@goobar.dev>
2022-02-20 16:47:45 -0700

    07_03

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,13 @@
+import java.io.File
+import kotlin.io.path.writeLines
 
 fun main() {
+    val scoresFile = File("scores.txt")
 
+    scoresFile.forEachLine { line -> println(line) }
+
+    val sortedLines = scoresFile.readLines().sorted()
+
+    val outputFile = File("sorted-scores.txt").toPath()
+    outputFile.writeLines(sortedLines)
 }
