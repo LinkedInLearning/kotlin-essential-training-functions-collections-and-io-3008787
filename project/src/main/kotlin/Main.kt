2a871f71e342849a5906f527998f506554559023
linkedin <n8ebel@goobar.dev>
2022-02-19 19:03:32 -0700

    Prep for 06_03

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,27 +1,4 @@
 
 fun main() {
-    val stringList: List<String> = listOf("kotlin", "code", "puppy")
-    val listOfInt = listOf(1, 2, 3, null)
 
-    val doubledValues = List<Int>(5) { index ->
-        2 * index
-    }
-
-    doubledValues.forEach { println(it) }
-
-    val emptyStringList: List<String> = emptyList()
-
-    val firstInt = listOfInt.get(0)
-    val secondedInt = listOfInt[1]
-
-    val languages = mutableListOf("kotlin", "java")
-    languages[0] = "Kotlin"
-    languages.set(0, "Kotlin")
-
-    languages.add("javascript")
-    languages.add(1, "c++")
-    languages.remove("javascript")
-    languages.removeAt(0)
-
-    val aList: List<String> = mutableListOf("dog", "cat")
 }
