fad130959f9f6cbb351f1eab980eebfdfac55bfb
linkedin <n8ebel@goobar.dev>
2022-03-13 10:36:09 -0600

    06_05

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
 
 fun main() {
-    
+    val languages = listOf("kotlin", "c++", "java")
+        .filter { it.length > 4 }
+        .map { it.length }
+
+    val languageSequence = sequenceOf("kotlin", "c++", "java")
+
+    val fromIterable = listOf(1, 2, 3).asSequence()
+
+    languageSequence.filter { it.length > 4 }.map { it.length }.take(2)
 }
